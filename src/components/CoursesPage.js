88770c1720b370eccd13a55e9d7b8dfad46a0495
Adi Koundinya <aditya.koundinya@dimensional.com>
2020-01-17 16:51:05 -0600

    Adding CoursePage

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { getCourses } from "../api/courseApi";
+import CourseList from "./CourseList";
 
 function CoursesPage() {
   const [courses, setCourses] = useState([]);
@@ -11,26 +12,7 @@ function CoursesPage() {
   return (
     <>
       <h2>Courses</h2>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Author</th>
-            <th>Category</th>
-          </tr>
-        </thead>
-        <tbody>
-          {courses.map(course => {
-            return (
-              <tr key={course.id}>
-                <td>{course.title}</td>
-                <td>{course.authorId}</td>
-                <td>{course.category}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <CourseList courses={courses} />
     </>
   );
 }
