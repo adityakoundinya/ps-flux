9c0c8ebdcd2c8d8fa53172a4c3c1dd6cb831d8b6
Aditya Koundinya <aditya.koundinya@gmail.com>
2020-01-19 18:46:00 -0600

    Adding Notfound and manageCourse with routes.

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,26 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 import Header from "./common/Header";
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
 import CoursesPage from "./CoursesPage";
+import NotFoundPage from "./NotFoundPage";
+import ManageCoursePage from "./ManageCoursePage";
 
 function App() {
-  function getPage() {
-    const route = window.location.pathname;
-    if (route === "/about") return <AboutPage />;
-    if (route === "/courses") return <CoursesPage />;
-    return <HomePage />;
-  }
   return (
     <div className="container-fluid">
       <Header />
-      {getPage()}
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/courses" component={CoursesPage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/course/:slug" component={ManageCoursePage} />
+        <Redirect from="/about-page" to="/about"></Redirect>
+        <Route component={NotFoundPage}></Route>
+      </Switch>
     </div>
   );
 }
